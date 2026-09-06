1833d15b828c6836d751859708a0cd8f21daf9b8
null-a0 <23f1001572@ds.study.iitm.ac.in>
2026-09-06 09:58:49 +0000

    Add wrk stress test script

---
+++ stress_test.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.body = '{"sno":87,"age":46,"gender":0,"cp":1,"trestbps":101,"chol":197,"fbs":1,"restecg":1,"thalach":156,"exang":0,"oldpeak":0,"slope":2,"ca":0,"thal":3}'
+wrk.headers["Content-Type"] = "application/json"
